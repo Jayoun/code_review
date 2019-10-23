95f32f04023ef1953ab5f597ebe91960d51230b4
Jayoun <airjany.lee@gmail.com>
2019-10-23 15:43:53 +0900

    Header Guard/Class 및 Function name 수정
    Class/Method Naming rule guide에 맞게 수정 반영
    TemplatePriorityQueue
    bool Empty() const;
    const T& Top() const;
    int Size();
    void Push(const T&);
    void Pop();
    header define guard 추가 : PROJECT1_PRIORITYQUEUE_H_

--- project1/priority_queue.h
+++ project1/priority_queue.h
@@ -1,15 +1,33 @@
+#ifndef PROJECT1_PRIORITYQUEUE_H_
+#define PROJECT1_PRIORITYQUEUE_H_
+
 #include <queue>
 
+// TemplatePriorityQueue class supports Priority Queue for template.
+// Example:
+//     templatePriorityQueue<int> expTarget;
+//     expTarget.Push(1);
+//     expTarget.Push(4);
+//     ...
+//     expTarget.Pop();
+//     expTarget.Pop();
+//     ...
+
+
 template <typename T> 
-class templatePriorityQueue {
+class TemplatePriorityQueue {
+	public:
+		TemplatePriorityQueue();
+		~TemplatePriorityQueue();
+		bool Empty() const;
+		const T& Top() const;
+		int Size();
+		void Push(const T&);
+		void Pop();
+
 	private:
 		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
 };
+
+
+#endif  // PROJECT1_PRIORITYQUEUE_H_
